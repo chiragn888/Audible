f4c944d4b060e343ef560ae750ac8ed30a314dd2
Chirag N <65002995+chiragn888@users.noreply.github.com>
2022-07-29 09:29:53 +0530

    Add files via upload

---
+++ audible.py
@@ -0,0 +1,52 @@
+from pdf2image import convert_from_path
+from PIL import Image
+from numpy import append
+import pytesseract
+import cv2
+from distutils.command.config import config
+from googletrans import Translator
+import os
+from text_to_speech import speak
+import pyttsx3
+import re
+import re
+import json
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+sent=""
+
+def talk(text):
+    speak(text,'en',file="audible.mp3",save=True, speak=True)
+
+filename=input()
+poppler_path = r'/opt/homebrew/Cellar/poppler/22.06.0/bin'
+pdf_path = filename
+images = convert_from_path(pdf_path=pdf_path, poppler_path=poppler_path)
+file_names=[]
+for count, img in enumerate(images):
+    img_name = f"page_{count+1}.png"
+    img.save(img_name, "PNG")
+    file_names.append(img_name)
+print(file_names)
+
+for file in file_names:
+    img=cv2.imread(file)
+    text=pytesseract.image_to_string(Image.open(file), lang='eng')
+    sent=sent+text
+with open('audible.txt', 'w', encoding='utf-8') as f:
+    print(sent, file=f)
+
+abcd=open("audible.txt",'r').read()
+print("speaking....")
+talk(abcd)    
+
+
+
+
+
+
+
+
+
